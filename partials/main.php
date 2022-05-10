778c4d1a75f55eb5fb9c5e8bbe7cc8dd2d2f212c
Ga <gabri.magni.97@gmail.com>
2022-05-10 15:59:52 +0200

    database

--- partials/main.php
+++ partials/main.php
@@ -1,35 +1,4 @@
-<?php 
-class Movie {
-    public $title;
-    public $rate;
-    public $genre;
-    public $director;
-    public $advice;
-
-    function __construct($_title, $_rate, $_genre, $_director) {
-        $this->title = $_title;
-        $this->rate = $_rate;
-        $this->genre = $_genre;
-        $this->director = $_director;
-    }
-
-    public function setConsigliato($rate) {
-        if($rate > 6) {
-            $this->advice = 'consigliato';
-        } else {
-            $this->advice = 'sconsigliato';
-        }
-    }
-
-    public function getConsigliato() {
-        return $this->advice;
-    }
-}
-
-$inglouriousBasterds = new Movie('Inglourious Basterds', 10, 'Action', 'Tarantino');
-$noCountryForOldMen = new Movie('No Country For Old Men', 10, 'Thriller', 'Coen bros.');
-var_dump($inglouriousBasterds)
-?>
+<?php require __DIR__ . '/database.php' ?>
 
 <div class="container">
     <div class="row">
